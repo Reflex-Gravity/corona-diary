9e627d04757ec44d07a029ea833faf70820de5b2
Joel DSouza <joel.dsouza@kapturecrm.com>
2021-07-12 04:36:47 +0530

    clean code in App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 // app.test.js
-import { render, screen, fireEvent } from './test-utils'
-import userEvent from '@testing-library/user-event'
+import { render, screen } from './test-utils'
 import { createMemoryHistory } from 'history'
 import React from 'react'
 import { Router } from 'react-router-dom'
@@ -10,24 +9,15 @@ import '@testing-library/jest-dom/extend-expect'
 import App from './App'
 import { waitFor } from '@testing-library/react'
 
-test('login to list page rendering', async () => {
+test('login page rendering', async () => {
     const history = createMemoryHistory()
-    const { getByLabelText } = render(
+    render(
         <Router history={history}>
             <App />
         </Router>
     )
-    // verify page content for expected route
-    // often you'd use a data-testid or role query, but this is also possible
+
     await waitFor(() => {
         expect(screen.getByText(/Your Name/i)).toBeInTheDocument()
     })
-
-    // const leftClick = { button: 0 }
-    // fireEvent.change(getByLabelText('Email'), { target: { value: 'Jane Doe' } })
-    // userEvent.click(screen.getByText('Login').closest('button'), leftClick)
-
-    // await waitFor(() => {
-    //     expect(screen.getByText(/Your visits/i)).toBeInTheDocument()
-    // })
 })
