64774d7b6a8f04dc2b84bb5b18e9c392e898dc47
Joel DSouza <joel.dsouza@kapturecrm.com>
2021-07-11 14:49:26 +0530

    Added back button in AddMeeting

--- src/main/AddMeeting.tsx
+++ src/main/AddMeeting.tsx
@@ -1,14 +1,23 @@
 import React, { useState } from 'react'
-import { Card, CardContent, Typography, TextField } from '@material-ui/core'
+import { Card, CardContent, Typography, TextField, IconButton, Icon, makeStyles, CardActions } from '@material-ui/core'
 import DayPickerInput from 'react-day-picker/DayPickerInput'
 import SelectPlaces from './components/SearchPlaces'
 import 'react-day-picker/lib/style.css'
 import { Button } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { addMeeting } from 'store/actions'
 
+import Logout from './components/Logout'
+
+const useStyles = makeStyles({
+    margin: {
+        marginTop: 5,
+    },
+})
+
 function DateInput(props: never) {
-    return <TextField label="Meeting Date" {...props} />
+    return <TextField className="mt-5" label="Meeting Date" {...props} />
 }
 
 const defaultLocation: GeolocationCoordinates = {
@@ -26,6 +35,8 @@ function AddMeeting(): JSX.Element {
     const [location, setLocation] = useState(defaultLocation)
     const [meetingDate, setMeetingDate] = useState(new Date())
     const dispatch = useDispatch()
+    const history = useHistory()
+    const classes = useStyles()
 
     function handleNameChange(evt: React.ChangeEvent<HTMLInputElement>) {
         setName(evt.target.value)
@@ -43,23 +54,36 @@ function AddMeeting(): JSX.Element {
         const meetingData = {
             name,
             location: `${location.latitude},${location.longitude}`,
-            date: meetingDate,
-            group_id: new Date().getTime(),
+            date: meetingDate.toLocaleDateString(),
+            id: new Date().getTime(),
         }
         dispatch(addMeeting(meetingData))
+        history.push('/list')
+    }
+
+    function goBack() {
+        history.push('/list')
     }
 
     return (
         <Card className="add-meeting-wrapper">
             <CardContent>
-                <Typography variant="h6">Add Meeting</Typography>
+                <CardActions>
+                    <IconButton onClick={goBack}>
+                        <Icon>arrow_back</Icon>
+                    </IconButton>
+                    <Typography variant="h6">Add Meeting</Typography>
+                </CardActions>
+                <Logout />
                 <div className="add-meeting-form flex flex-col">
-                    <TextField label="Person Name" value={name} onChange={handleNameChange} />
+                    <TextField className="mt-5" label="Person Name" value={name} onChange={handleNameChange} />
                     <DayPickerInput component={DateInput} value={meetingDate} onDayChange={handleDateChange} />
-                    <SelectPlaces onSelect={handleLocation} />
-                    <Typography>
-                        Location: {location.latitude},{location.longitude}
-                    </Typography>
+                    <div className="flex items-center mt-5">
+                        <Typography>
+                            Location: {location.latitude},{location.longitude}
+                        </Typography>
+                        <SelectPlaces onSelect={handleLocation} />
+                    </div>
                 </div>
                 <Button className="meeting-submit-btn" color="primary" variant="contained" onClick={handleAddMeeting}>
                     Add
